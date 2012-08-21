5592800524b31bc37054ad17e21210a294001b96
Chris Reeves <me@chris-reeves.com>
2012-08-21 17:11:07 +0000

    Changed set_project_name method to be more generic and set general cli args

--- tests/test_config.py
+++ tests/test_config.py
@@ -16,9 +16,9 @@ class ConfigTests(unittest.TestCase):
     def tearDown(self):
         sys.argv = self._old_sys_argv
 
-    def _set_project_name(self, name):
+    def _set_cli_args(self, args):
         with nostdout():
-            sys.argv = ['', '-n', name]
+            sys.argv = sys.argv + args
             self.config = Config()
 
     def should_exit_with_no_arguments(self):
@@ -29,21 +29,21 @@ class ConfigTests(unittest.TestCase):
             assert True
 
     def ensure_valid_project_name(self):
-        self._set_project_name('this_is_valid')
+        self._set_cli_args(['-n', 'this_is_valid'])
         self.assertEquals(self.config.project_name, 'this_is_valid')
-        self._set_project_name('Thisisvalid')
+        self._set_cli_args(['-n', 'Thisisvalid'])
         self.assertEquals(self.config.project_name, 'Thisisvalid')
 
     def should_exit_on_invalid_name(self):
         try:
-            self._set_project_name('this_is_not-valid')
+            self._set_cli_args(['-n', 'this_is_not-valid'])
         except SystemExit:
             assert True
         try:
-            self._set_project_name('this_is not_valid')
+            self._set_cli_args(['-n', 'this_is not_valid'])
         except SystemExit:
             assert True
         try:
-            self._set_project_name('*this_is_not_valid')
+            self._set_cli_args(['-n', '*this_is_not_valid'])
         except SystemExit:
             assert True
