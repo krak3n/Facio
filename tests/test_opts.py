15c914bc89251159c2a15564ca7b6996c61684e4
Chris Reeves <me@chris-reeves.com>
2012-10-16 18:18:03 +0000

    Opt testing stub

---
+++ tests/test_opts.py
@@ -0,0 +1,13 @@
+import sys
+
+from skeletor import config
+from skeletor.config import Config
+
+from .base import BaseTestCase
+from .helpers import nostdout
+
+
+class OptsTests(BaseTestCase):
+
+    def test_something(self):
+        assert True
