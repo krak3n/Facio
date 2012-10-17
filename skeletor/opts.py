59b649fada7a3b0fe8af84d26489c2a492864e33
Chris Reeves <me@chris-reeves.com>
2012-10-17 08:30:05 +0000

    Pep8 compliance

--- skeletor/opts.py
+++ skeletor/opts.py
@@ -8,10 +8,11 @@ http://docs.python.org/release/2.3/lib/optparse-extending-examples.html.
 
 import optparse
 
+
 class Option(optparse.Option):
     ATTRS = optparse.Option.ATTRS + ['required']
 
-    def _check_required (self):
+    def _check_required(self):
         if self.required and not self.takes_value():
             raise optparse.OptionError(
                 "required flag set for option that doesn't take a value",
@@ -20,17 +21,18 @@ class Option(optparse.Option):
     # Make sure _check_required() is called from the constructor!
     CHECK_METHODS = optparse.Option.CHECK_METHODS + [_check_required]
 
-    def process (self, opt, value, values, parser):
+    def process(self, opt, value, values, parser):
         optparse.Option.process(self, opt, value, values, parser)
         parser.option_seen[self] = 1
 
+
 class OptionParser(optparse.OptionParser):
 
-    def _init_parsing_state (self):
+    def _init_parsing_state(self):
         optparse.OptionParser._init_parsing_state(self)
         self.option_seen = {}
 
-    def check_values (self, values, args):
+    def check_values(self, values, args):
         for option in self.option_list:
             if (isinstance(option, Option) and
                 option.required and
