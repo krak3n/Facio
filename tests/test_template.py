79716d6b302890c69f43a1ea96cc46093ceb65ae
Chris Reeves <me@chris-reeves.com>
2012-11-09 16:48:49 +0000

    First test using Mock to ensure mal formed variabels are not added to Template placeholder dict

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,8 +1,15 @@
+from mock import MagicMock
+from skeletor.template import Template
+
 from .base import BaseTestCase
 
 
 class TemplateTests(BaseTestCase):
-    """ Template Tests. """
+    """ Template Tests """
 
-    def test_something(self):
-        assert True
+    def should_handle_malformed_variables_gracefully(self):
+        config = MagicMock(name='config')
+        config.variables = 'xy,y'  # Malformed custom variables
+        config.project_name = 'test_project'
+        t = Template(config)
+        self.assertEquals(len(t.place_holders), 6)
