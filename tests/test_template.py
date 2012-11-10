ef8914efc113ae8ff36df2ef257bba390d71f740
Chris Reeves <me@chris-reeves.com>
2012-11-10 20:28:29 +0000

    First tempalte test - ensureing custom variables are set correctly

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,15 +1,29 @@
+import unittest
+
 from mock import MagicMock
 from skeletor.template import Template
 
-from .base import BaseTestCase
 
-
-class TemplateTests(BaseTestCase):
+class TemplateTests(unittest.TestCase):
     """ Template Tests """
 
+    def setUp(self):
+        self.config = MagicMock(name='config')
+        self.config.project_name = 'test_project'
+        self.config.django_secret_key = 'xxx'
+        self.config.template_settings_dir = 'settings'
+
     def should_handle_malformed_variables_gracefully(self):
-        config = MagicMock(name='config')
-        config.variables = 'xy,y'  # Malformed custom variables
-        config.project_name = 'test_project'
-        t = Template(config)
-        self.assertEquals(len(t.place_holders), 6)
+        self.config.variables = 'this,is.wrong'
+        t = Template(self.config)
+
+        self.assertEquals(len(t.place_holders), 3)
+
+    def ensure_custom_variables_added_to_placeholders(self):
+        self.config.variables = 'foo=bar,baz=1'
+        t = Template(self.config)
+
+        self.assertTrue('foo' in t.place_holders)
+        self.assertEquals(t.place_holders['foo'], 'bar')
+        self.assertTrue('baz' in t.place_holders)
+        self.assertEquals(t.place_holders['baz'], '1')
