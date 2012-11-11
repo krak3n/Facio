f002e7f9f3b0ae3af4e61c779279056e80c4437f
Chris Reeves <me@chris-reeves.com>
2012-11-11 00:14:45 +0000

    Unique project names for each test

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,4 +1,6 @@
+import os
 import unittest
+import uuid
 
 from mock import MagicMock
 from skeletor.template import Template
@@ -8,10 +10,14 @@ class TemplateTests(unittest.TestCase):
     """ Template Tests """
 
     def setUp(self):
+        # Mock out the config class
         self.config = MagicMock(name='config')
-        self.config.project_name = 'test_project'
+        self.config.project_name = uuid.uuid4().hex  # Random project name
         self.config.django_secret_key = 'xxx'
         self.config.template_settings_dir = 'settings'
+        self.config.cli_opts.error = MagicMock(side_effect=Exception)
+        self.config.template = os.path.join(os.path.dirname(
+            os.path.realpath(__file__)), 'test_template')
 
     def should_handle_malformed_variables_gracefully(self):
         self.config.variables = 'this,is.wrong'
