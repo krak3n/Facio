03e777043aea595d9986a35ea2eb857c06faf2ab
Chris Reeves <me@chris-reeves.com>
2012-11-25 18:06:19 +0000

    Updating setup with classifiers

--- setup.py
+++ setup.py
@@ -1,19 +1,41 @@
 #!/usr/bin/env python
-# encoding: utf-8
+"""
+Skeletor
+========
+
+Skeletor is a project scaffolding tool origionally developed for Django
+and expanded to be framework agnostic. You can use Skeletor to bootstrap
+any sort of project.
+"""
 
 from setuptools import setup, find_packages
-from skeletor import VERSION
+from skeletor import __version__
+
+
+install_requires = ['GitPython==0.3.2.RC1', 'Jinja2==2.6']
 
 setup(
-    name="Skeletor",
-    version=VERSION,
-    url='https://github.com/krak3n/Skeletor',
-    author='Christopher Reeves',
+    name='skeletor',
+    version=__version__,
+    author='Christopher John Reeves',
     author_email='hello@chris.reeves.io',
-    description='A django project skeleton generator similar to django '
-                'startproject',
-    packages=find_packages(exclude=['examples', 'tests']),
-    install_requires=['GitPython==0.3.2.RC1', 'Jinja2==2.6'],
+    url='http://chris.reeves.io',
+    description='Project scaffolding from custom templates.',
+    long_description=__doc__,
+    zip_safe=False,
+    packages=find_packages(exclude=['tests', ]),
+    install_requires=install_requires,
     scripts=['skeletor/bin/skeletor'],
-    zip_safe=False
+    classifiers=[
+        'Environment :: Console',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Developers',
+        'Operating System :: Unix'
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
