076a238cf94ee2c92836f912211b97b5242f2bf2
Chris Reeves <hello@chris.reeves.io>
2013-05-19 20:44:04 +0000

    Fixing run tests script

--- runtests.py
+++ runtests.py
@@ -5,11 +5,8 @@ def runtests():
     import nose
     argv = []
     argv.insert(1, '')
-    argv.insert(2, '--with-spec')
-    argv.insert(3, '--spec-color')
-    argv.insert(4, '--with-coverage')
-    argv.insert(5, '--cover-erase')
-    argv.insert(6, '--cover-package=facio')
+    argv.insert(2, '--with-coverage')
+    argv.insert(3, '--cover-package=facio')
     nose.main(argv=argv)
 
 
