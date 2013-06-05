a062910df8eec49355464760c3e3dbc496cc86f5
Chris Reeves <hello@chris.reeves.io>
2013-06-05 18:18:02 +0000

    Extracting git tests out of template test to seperate tests file

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,3 +1,8 @@
+"""
+.. module:: tests.template
+   :synopsis: Unit tests for template module
+"""
+
 import os
 import tempfile
 import uuid
@@ -5,7 +10,6 @@ import uuid
 from codecs import open
 from facio.template import Template
 from mock import MagicMock, PropertyMock, patch
-from sh import git
 from shutil import rmtree
 from six import StringIO
 
@@ -28,9 +32,6 @@ class TemplateTests(BaseTestCase):
         self.puts_patch = patch('facio.template.puts',
                                 stream=StringIO)
         self.puts_patch.start()
-        self.puts_patch_vcs = patch('facio.vcs.git.puts',
-                                    stream=StringIO)
-        self.puts_patch_vcs.start()
 
     def test_handle_malformed_variables_gracefully(self):
         self.config.variables = 'this,is.wrong'
@@ -91,56 +92,6 @@ class TemplateTests(BaseTestCase):
         self.config.template = 'git+git@somewhere.com:repo.git'
         t = Template(self.config)
         self.assertEquals(t.vcs_cls.__class__.__name__, 'Git')
-
-    @patch('facio.template.Template.working_dir', new_callable=PropertyMock)
-    def test_clone_git_repo(self, mock_working_dir):
-
-        # Create a fake temp repo w/ commit
-        git_dir = tempfile.mkdtemp(prefix='git')
-        git.init(git_dir)
-
-        try:
-            f = open(os.path.join(git_dir, 'foo.txt'), 'w')
-            f.write('blah')
-        except IOError:
-            assert False
-        finally:
-            f.close()
-        cwd = os.getcwd()
-        os.chdir(git_dir)
-        git.add('foo.txt')
-        if os.environ.get('TRAVIS_CI') == 'true':
-            git.config('--global', 'user.email',
-                       '"auto-version-test@travis.ci"')
-            git.config('--global', 'user.name',
-                       '"Testing on Travis CI"')
-        git.commit('-m "Added foo.txt"')
-        os.chdir(cwd)
-
-        # Fake Template Path
-        mock_working_dir.return_value = tempfile.gettempdir()
-
-        # Now attempt to clone but patch for Exception throw
-        self.config.template = 'git+' + git_dir
-        self.config._tpl = git_dir
-        t = Template(self.config)
-        t.copy_template()
-
-        # Clean up
-        rmtree(t.project_root)
-
-        self.assertFalse(os.path.isdir(t.config.template))
-        self.assertFalse(self.config.cli_opts.error.called)
-
-    @patch('sh.git.clone', side_effect=Exception)
-    def test_error_on_clone_exception(self, mock_repo_init):
-        self.config.template = 'git+this/wont/work'
-        try:
-            Template(self.config)
-        except:
-            assert True
-        else:
-            assert False
 
     @patch('os.path.isdir', return_value=False)
     @patch('facio.config.Config._error')
