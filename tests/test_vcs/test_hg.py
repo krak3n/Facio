aa2cfdedbeeb14c93869f62ca87d5dd48dd284fb
Chris Reeves <hello@chris.reeves.io>
2013-06-06 12:34:17 +0000

    Mercurial tests module - not yet complete

--- tests/test_vcs/test_hg.py
+++ tests/test_vcs/test_hg.py
@@ -1,6 +1,6 @@
 """
-.. module:: tests.test_vcs.git
-   :synopsis: Unit tests for the git vcs module
+.. module:: tests.test_vcs.hg
+   :synopsis: Unit tests for the hg vcs module
 """
 
 import os
@@ -9,7 +9,7 @@ import uuid
 
 from facio.template import Template
 from mock import MagicMock, PropertyMock, patch
-from sh import git
+from sh import hg
 from six import StringIO
 from shutil import rmtree
 
@@ -33,3 +33,64 @@ class HgTests(BaseTestCase):
             'facio.vcs.hg.puts',
             stream=StringIO)
         self.puts_patch_vcs.start()
+
+    @patch('sh.Environment.__getitem__', side_effect=ImportError)
+    def test_mercurial_import_fails_if_missing(self, mock_sh):
+        self.config.template = 'hg+this/wont/work'
+        try:
+            Template(self.config)
+        except ImportError:
+            assert True
+        else:
+            assert False
+
+    @patch('sh.hg.clone', side_effect=Exception)
+    def test_error_on_clone_exception(self, mock_repo_init):
+        self.config.template = 'hg+this/wont/work'
+        try:
+            Template(self.config)
+        except:
+            assert True
+        else:
+            assert False
+
+    @patch('facio.template.Template.working_dir', new_callable=PropertyMock)
+    def test_clone_hg_repo(self, mock_working_dir):
+        cwd = os.getcwd()
+
+        if os.environ.get('TRAVIS_CI') == 'true':
+            # Set hg users etc
+            pass
+
+        # Create a fake temp repo w/ commit
+        hg_dir = tempfile.mkdtemp(prefix='hg')
+        hg.init(hg_dir)
+
+        try:
+            f = open(os.path.join(hg_dir, 'foo.txt'), 'w')
+            f.write('blah')
+        except IOError:
+            assert False
+        finally:
+            f.close()
+
+        os.chdir(hg_dir)
+        hg.add('foo.txt')
+        hg.commit('-m "Added foo.txt"')
+
+        os.chdir(cwd)
+
+        # Fake Template Path
+        mock_working_dir.return_value = tempfile.gettempdir()
+
+        # Now attempt to clone but patch for Exception throw
+        self.config.template = 'hg+' + hg_dir
+        self.config._tpl = hg_dir
+        t = Template(self.config)
+        t.copy_template()
+
+        # Clean up
+        rmtree(t.project_root)
+
+        self.assertFalse(os.path.isdir(t.config.template))
+        self.assertFalse(self.config.cli_opts.error.called)
