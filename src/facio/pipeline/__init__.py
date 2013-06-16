9f822d89a8ea55ab3632a939bfc90deeb7c02577
Chris Reeves <hello@chris.reeves.io>
2013-06-16 16:46:17 +0000

    Base pipeline class and yml parsing

---
+++ src/facio/pipeline/__init__.py
@@ -0,0 +1,33 @@
+"""
+.. module:: facio.pipeline
+   :synopsis: Pipeline detection and execution.
+"""
+
+import yaml
+
+from clint.textui import puts
+from yaml.scanner import ScannerError
+
+
+class Pipeline(object):
+
+    def __init__(self, tpl_class):
+        """ Pipeline class instanctiation.
+
+        :param tpl_class: Template class instance,
+        :type tpl_class: Object
+        """
+
+        self.tpl = tpl_class
+        self._parse()
+
+    def _parse(self):
+        """ Parse the pipeline file. """
+
+        with open(self.tpl.pipeline_file) as f:
+            try:
+                self.pipeline = yaml.load(f)
+            except ScannerError:
+                puts("Error loading Pipeline - Is it correctly formatted?")
+            else:
+                puts("Loading Pipeline")
