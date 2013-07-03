b0c1542ae6da2ea60cce30d4ab82e0baf3b2b75e
Chris Reeves <hello@chris.reeves.io>
2013-07-03 22:13:42 +0000

    Facio start command now calling refactored template class methods

--- src/facio/start.py
+++ src/facio/start.py
@@ -9,7 +9,7 @@ from .config import Settings, CommandLineInterface, ConfigurationFile
 from .template import Template
 
 from clint.textui import puts, indent
-from clint.textui.colored import green
+from clint.textui.colored import blue, green
 
 
 class Start(object):
@@ -23,13 +23,17 @@ class Start(object):
         parsed = config.read()
 
         settings = Settings(interface, parsed)
-        path = settings.get_template_path()
 
-        puts(path)
+        template = Template(
+            settings.get_project_name(),
+            settings.get_template_path()
+        )
+
+        template.update_ignore_globs(settings.get_variables())
+        template.update_ignore_globs(settings.get_ignore_globs())
+
+        template.copy()
+        template.rename()
 
         with indent(4, quote=' >'):
             puts(green('Done'))
-
-    def process_template(self):
-        self.template = Template(self.config)
-        self.template.copy_template()
