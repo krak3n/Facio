b0693954c1473d7dd21aba4885477798fef11eff
Chris Reeves <hello@chris.reeves.io>
2013-07-06 23:10:13 +0000

    Tests for base facio class

---
+++ tests/test_base.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: tests.test_base
+   :synopsis: Tests for Facio base class
+"""
+
+from facio import Facio
+
+from . import BaseTestCase
+
+
+class TestFacio(BaseTestCase):
+
+    def setUp(self):
+        self._patch_clint([
+            'facio.base.puts',
+        ])
+
+    def test_out(self):
+        f = Facio()
+        f.out('Foo')
+
+        self.mocked_facio_base_puts.called_once_with('Foo')
+
+    def test_warning(self):
+        f = Facio()
+        f.warning('Foo')
+
+        self.mocked_facio_base_puts.called_once_with('Warning: Foo')
+
+    def test_error(self):
+        f = Facio()
+        f.error('Foo')
+
+        self.mocked_facio_base_puts.called_once_with('Error: Foo')
+
+    def test_success(self):
+        f = Facio()
+        f.success('Foo')
+
+        self.mocked_facio_base_puts.called_once_with('Error: Foo')
