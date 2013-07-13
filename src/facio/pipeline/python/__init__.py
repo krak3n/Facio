d28d3e90f18a892601a5ad6645ad706b27428579
Chris Reeves <hello@chris.reeves.io>
2013-07-13 14:50:33 +0000

    Stubbing out modules

---
+++ src/facio/pipeline/python/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: facio.pipeline.python
+   :synopsis: Bundled python pipelines.
+"""
---
+++ src/facio/pipeline/python/virtualenv.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: facio.pipeline.python.virtualenv
+   :synopsis: Bundled pipeline for creating python virtual environments.
+"""
