036483592a239d9cc140232ef1b1ca6e34fc8b75
Chris Reeves <hello@chris.reeves.io>
2013-07-13 15:09:38 +0000

    Stubbing out the methods we will need

--- src/facio/pipeline/python/virtualenv.py
+++ src/facio/pipeline/python/virtualenv.py
@@ -4,3 +4,36 @@
 .. module:: facio.pipeline.python.virtualenv
    :synopsis: Bundled pipeline for creating python virtual environments.
 """
+
+from .. import BaseFacio
+
+
+class Virtualenv(BaseFacio):
+
+    def get_name(self):
+        """ Returns the name for the virtualenv - gathered from user input with
+        the default value being the project name from facio state.
+        """
+
+        pass
+
+    def get_path(self):
+        """ The path to where the virtual environment should be created, the
+        user is prompted to input this path, default will be ~/.virtualenvs.
+        """
+
+        pass
+
+    def create(self):
+        """ Creates a python virtual environment. """
+
+        pass
+
+
+def run():
+    """ Called from ``facio.pipeline`` runner.
+
+    :returns: str -- Path to the created virtual environment
+    """
+
+    pass
