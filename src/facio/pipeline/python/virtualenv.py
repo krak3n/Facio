763e3cb979ff4533756854cf2530775d926fc5a0
Chris Reeves <hello@chris.reeves.io>
2013-07-15 20:13:50 +0000

    Implementation of retrieving virtual environment name from the user

--- src/facio/pipeline/python/virtualenv.py
+++ src/facio/pipeline/python/virtualenv.py
@@ -5,7 +5,8 @@
    :synopsis: Bundled pipeline for creating python virtual environments.
 """
 
-from .. import BaseFacio
+from facio.base import BaseFacio
+from facio.state import state
 
 
 class Virtualenv(BaseFacio):
@@ -13,9 +14,18 @@ class Virtualenv(BaseFacio):
     def get_name(self):
         """ Returns the name for the virtualenv - gathered from user input with
         the default value being the project name from facio state.
+
+        :returns: str -- Virtual environment name
         """
 
-        pass
+        project_name = state.get_context_variable('PROJECT_NAME')
+        prompt = "Please enter a name for the virtual environment you want "\
+                 "to create, leave blank to name it {0}: ".format(project_name)
+        name = self.gather(prompt)
+        if not name:
+            name = project_name
+
+        return name
 
     def get_path(self):
         """ The path to where the virtual environment should be created, the
