090cb3937be6514256220bd92f7f82ca5389492f
Chris Reeves <hello@chris.reeves.io>
2013-07-17 17:47:38 +0000

    Implementation of getting the default python path

--- src/facio/pipeline/python/setup.py
+++ src/facio/pipeline/python/setup.py
@@ -5,7 +5,10 @@
    :synopsis: Bundled pipeline for running python setup.py
 """
 
+import sys
+
 from facio.base import BaseFacio
+from facio.state import state
 
 
 class Setup(BaseFacio):
@@ -30,6 +33,23 @@ class Setup(BaseFacio):
             return None
 
         return arg
+
+    def get_default_path_to_pyth(self):
+        """ Returns the default path to python, if virtualenv pipeline
+        has been called use that path, else use the current executing
+        python, this should be the systems python in most cases.
+
+        :returns: str -- path to python executable
+        """
+
+        # Returns path to virtualenv
+        call = state.pipeline_get_call_result(
+            'facio.pipeline.python.virtualenv')
+
+        if call:
+            return call
+        else:
+            return sys.executable
 
     def get_path_to_python(self):
         """ Gets the path to python to run setup.py against.
